b0065a9fdf8983b15a1cc45dc5c19d5aaa0e1000
mujib2953 <ansarimujiburrehman@gmail.com>
2020-01-04 01:05:32 +0530

    Added basic read and write to file through nodejs.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 const express = require('express');
+const fs = require("fs");
+
 const router = express.Router();
+
+const FILE_NAME = "files/waste.txt";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Express' });
+	try {
+		fs.readFile(FILE_NAME, (e, data) => {
+			if (e) throw e;
+
+			res.render('index', { data });
+		});
+	} catch(e) {
+		console.error(`Error occured while readig file: ${FILE_NAME}`, e);
+		res.render('index', { error: 'Unable to read file' });
+	}
+});
+
+router.post("/", (req, res) => {
+	const { data } = req.body;
+
+	try {
+		if (!data) throw new Error("No data found.");
+
+		fs.writeFile(FILE_NAME, data, (e) => {
+			if (e) throw e;
+
+			res.render("index", { data, isSaved: true });
+		});
+
+	} catch(e) {
+		console.error(`Error occured while writing file: ${FILE_NAME}`, e);
+		res.render('index', { error: 'Unable to write file' });
+	}
 });
 
 module.exports = router;

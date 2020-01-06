6475266580481850b28cdaba1d9cff8ad63dd602
mujib2953 <ansarimujiburrehman@gmail.com>
2020-01-06 18:12:19 +0530

    Implemented bold, italic and underline commands.

--- public/javascripts/custom/actionbar.js
+++ public/javascripts/custom/actionbar.js
@@ -1,6 +1,9 @@
 (() => {
     const actionBar = document.querySelector(".action-bar");
     const allActionButtons = actionBar.querySelectorAll("button");
+    const actionLists = [
+        "bold", "italic", "underline"
+    ];
 
     addEvents();
 
@@ -12,14 +15,20 @@
 
     function onActionClick(e) {
         const { target } = e;
+        const selecetedText = window.getSelection();
 
         target.classList.toggle("active");
 
-        // if (target.classList.contains("active")) {
-        //     target.classList.remove("active");
-        // } else {
-        //     target.classList.add("active");
-        // }
+        if (selecetedText.type.toLowerCase() === "none") {
+            return false;
+        }
+
+        const { professor: { value } } = target.attributes;
+        const hasAction = actionLists.find(act => act === value.toLowerCase());
+
+        if (hasAction) {
+            document.execCommand(value.toLowerCase());
+        }
     }
 
 })();

6317bde4e96c79e7cf0c8405631a7e553765d288
mujib2953 <ansarimujiburrehman@gmail.com>
2020-01-14 18:52:06 +0530

    Implemented cors in application.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -18,6 +19,9 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// --- cors related
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
